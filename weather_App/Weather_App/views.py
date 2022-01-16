fcbfa27ad6ab7f0d268596c3288e4f12663ab6d2
Melek <maibalonn@gmail.com>
2022-01-16 20:19:52 +0300

    models created

--- weather_App/Weather_App/views.py
+++ weather_App/Weather_App/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render,redirect
-
+import requests
+from decouple import config
+from pprint import pprint
 # Create your views here.
 def index(request):
+    
+    url='https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+    city="istanbul"
+    response=requests.get(url.format(city,config('API_KEY')))#url'e istek atıyoruz.
+    content=response.json()
+    pprint(content)
     return render(request,'Weather_App/index.html')
