1233f4a85af02f7c1c5445133540b398acb92b94
Melek <maibalonn@gmail.com>
2022-01-19 15:51:42 +0300

    api view occured

--- Serializers_functionbase_todo/apiTodo/views.py
+++ Serializers_functionbase_todo/apiTodo/views.py
@@ -4,11 +4,16 @@ from .serializers import TodoSerializer
 from .models import Todo
 
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 
 # Create your views here.
 def home(request):
     return HttpResponse('<center><h1 style="background-color:powderblue;">Welcome to ApiTodo<h1><center>')
 
-@api_view()
-def todoList
+@api_view(['GET'])#Httpresponsedan bana sadece GET gelirse cevap veririm, başka hiçbirtürlü cevap vermem
+def todoList(request):
+    query=Todo.objects.all()
+    serializer=TodoSerializer(query,many=True)#queryset formatında gelen To modeline ait elemanların bilgisini json formatına dönüştürmek için<-:
+    return Response(serializer.data)#bizim şuan frontentle işimiz yok o yüzden template gerek yok,biz sadece api olusturuyoruz oyüzden serializer değişkeni içine atadıgımız json formatındaki bilgiden bizim işimize yarayacak olan data kısmını alıyoruz.
