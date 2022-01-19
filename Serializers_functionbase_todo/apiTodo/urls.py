ab432144996fbc18ce21a4aa9ac6b3bef18d5fba
Melek <maibalonn@gmail.com>
2022-01-19 19:48:32 +0300

    api view created with GET and POST

--- Serializers_functionbase_todo/apiTodo/urls.py
+++ Serializers_functionbase_todo/apiTodo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home,todoList,todoListCreate
+from .views import home,todoList,todoListCreate,todo_List
 
 urlpatterns = [
     path('',home ),
     path('todoList/',todoList),
     path('todoListCreate/',todoListCreate),
+    path('todo_List/',todo_List),
 ]
--- Serializers_functionbase_todo/apiTodo/views.py
+++ Serializers_functionbase_todo/apiTodo/views.py
@@ -5,6 +5,7 @@ from .models import Todo
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 
@@ -23,4 +24,18 @@ def todoListCreate(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['GET','POST'])
+def todo_List(request):
+    if request.method=='GET':
+        query=Todo.objects.all()
+        serializer=TodoSerializer(query,many=True)
+        return Response(serializer.data)
+    if request.method=='POST':
+        serializer=TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
     
