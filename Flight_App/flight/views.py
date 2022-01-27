bfe2574778abc51a689dea1ad9e9123fa570c00f
Melek <maibalonn@gmail.com>
2022-01-27 19:53:28 +0300

    get_queryset overighted

--- Flight_App/flight/views.py
+++ Flight_App/flight/views.py
@@ -16,4 +16,10 @@ class ReservationView(viewsets.ModelViewSet):
     queryset=Reservation.objects.all()
     serializer_class=ReservationSerializer  
     
+    def get_queryset(self):#kullanıcı is_staff ise bütün rezervasyonlatı görsün is_staff değilse görmesin istiyorum.Bunun için querysete bir condition yazmam lazım(bunun için get_queryset methodunu overright ediyoruz)
+        queryset=super().get_queryset()#resercationview deki queryseti bize döndürüyor
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(user=self.request.user)
+    
 
