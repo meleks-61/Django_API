1e658b5b52f2690dcad6cf07aa0f0ee6eb170adc
Melek <maibalonn@gmail.com>
2022-01-27 20:12:12 +0300

    search created

--- Flight_App/flight/views.py
+++ Flight_App/flight/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Flight,Reservation,Passenger
 from .serializers import FligtSerializer,ReservationSerializer
-from rest_framework import viewsets#modelviewset kullanavağız
+from rest_framework import viewsets,filters#viewsetsi modelviewset kullanavağız,filter da search için
 from .permissions import IsStuffOrReadOnly
 
 
@@ -11,15 +11,17 @@ class FlightView(viewsets.ModelViewSet):#tüm işlemleri(get,put,post,delete,pat
     queryset=Flight.objects.all()
     serializer_class=FligtSerializer
     permission_classes=(IsStuffOrReadOnly,)#permission tanımlamalıyım.Admin ise,bu işlemleri yapacak kişi ise create,update işlemlerini yapsın ,onun haricinde kim varsa;authenticated olsun veya olmasın sisteme giren kişiler sadece read (flightları görebilsin).Her girenin create Update yapmaması lazım
-
+    filter_backends=(filters.SearchFilter,)#search için
+    search_fields=('departureCity','arrivalCity','dateOfDeparture')#search için
+    
 class ReservationView(viewsets.ModelViewSet):
     queryset=Reservation.objects.all()
     serializer_class=ReservationSerializer  
     
-    def get_queryset(self):#kullanıcı is_staff ise bütün rezervasyonlatı görsün is_staff değilse görmesin istiyorum.Bunun için querysete bir condition yazmam lazım(bunun için get_queryset methodunu overright ediyoruz)
+    def get_queryset(self):#kullanıcı is_staff ise bütün rezervasyonlatı görsün is_staff değilse görmesin istiyorum.Bunun için querysete bir condition yazmam lazım(bunun için get_queryset methodunu overright ediyoruz)eğer overright işlemi varsa super ile alıyoruz veriyi(pythonun object orianted olması ile ilgili bir durum bu)
         queryset=super().get_queryset()#resercationview deki queryseti bize döndürüyor
         if self.request.user.is_staff:
             return queryset
         return queryset.filter(user=self.request.user)
     
-
+#kullanıcı is_staff ise bir uçusa ait tüm rezervasyonları görsün.
